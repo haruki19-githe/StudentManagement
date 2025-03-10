f0011ccabce6ffc5b2be85b121447985232963f8
Fukuta Haruki <fukutaharuki@fukudaharukaonorenoMacBook-Air.local>
2025-03-10 15:31:12 +0900

    23回目の課題提出

---
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -0,0 +1,23 @@
+package raisetech.StudentManagement.repository;
+
+import org.apache.ibatis.annotations.*;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+
+import java.util.List;
+
+
+
+@Mapper
+public interface StudentRepository {
+
+    @Select("SELECT * FROM students")
+    List<Student>search();
+
+    @Select("SELECT * FROM students_courses")
+    List<StudentsCourses> searchStudentsCourses();
+
+
+}
+
+
